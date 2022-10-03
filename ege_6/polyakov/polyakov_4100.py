d9a52cfcffbd8c6e5bad407214d17e5392e84e9c
KruzhokProg <Aa1@3$5^7>
2022-10-03 10:06:38 +0300

    задача №2 решу егэ

--- ege_6/polyakov/polyakov_4100.py
+++ ege_6/polyakov/polyakov_4100.py
@@ -7,9 +7,9 @@ for s in range(1000):
         s1 = s1 + 3
         n = n - 6
     if n == 210:
-      if s > max:
-        max = s
-      if s < min:
-        min = s
+        if s > max:
+            max = s
+        if s < min:
+            min = s
 
 print(max, min)
